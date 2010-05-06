dbac503fe26fa6c112b1c0bae30644264702435d
Alan Gutierrez <alan@blogometer.com>
2010-05-06 14:07:04 -0500

    Javadoc for Not.
    [#3 state:open]

--- src/main/java/com/goodworkalan/deviate/Not.java
+++ src/main/java/com/goodworkalan/deviate/Not.java
@@ -1,36 +1,60 @@
 package com.goodworkalan.deviate;
 
-// TODO Document.
-public class Not implements Condition
-{
-    // TODO Document.
+/**
+ * Negates a condition returning true if the contained condition is false.
+ * 
+ * @author Alan Gutierrez
+ */
+public class Not implements Condition {
+    /** The condition to negate. */
     private final Condition condition;
-    
-    // TODO Document.
-    public Not(Condition condition)
-    {
+
+    /**
+     * Return true if the given condition evaluates to false.
+     * 
+     * @param condition
+     *            The condition to negate.
+     */
+    public Not(Condition condition) {
         this.condition = condition;
     }
-    
-    // TODO Document.
-    public boolean test(Object object)
-    {
+
+    /**
+     * Return true if evaluating the evaluation of the contained condition with
+     * the given object returns false.
+     * 
+     * @return The negated result of the contained condition evaluation.
+     */
+    public boolean test(Object object) {
         return !condition.test(object);
     }
-    
-    // TODO Document.
+
+    /**
+     * This object is equal to the given object if the given object is also an
+     * instance of <code>Not</code> and the contained conditions are equal.
+     * 
+     * @return True if this object is equalt to the given object.
+     */
     @Override
-    public boolean equals(Object object)
-    {
-        if (object == this)
-        {
+    public boolean equals(Object object) {
+        if (object == this) {
             return true;
         }
-        if (object instanceof Not)
-        {
+        if (object instanceof Not) {
             Not not = (Not) object;
             return condition.equals(not.condition);
         }
         return false;
+    }
+    
+    /**
+     * Creates a hash code by returning the hash code of the contained
+     * condition.
+     * 
+     * @return The hash code.
+     */
+    @Override
+    public int hashCode() {
+        return condition.hashCode();
     }
 }
