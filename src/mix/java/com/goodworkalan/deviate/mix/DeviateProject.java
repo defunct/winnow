fa7b7fd1534ff4183a5b13cd129f8529a56b2e4b
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:13:12 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/deviate/mix/DeviateProject.java
+++ src/mix/java/com/goodworkalan/deviate/mix/DeviateProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class DeviateProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Deviate.
+ *
+ * @author Alan Gutierrez
+ */
+public class DeviateProject implements ProjectModule {
+    /**
+     * Build the project definition for Deviate.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.deviate/deviate/0.1")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
