fc75a4530719843b30b334fe4bc3b7ecc03c26d3
Alan Gutierrez <alan@blogometer.com>
2010-06-07 09:51:07 -0500

    Added some Javadoc.

--- src/main/java/com/goodworkalan/deviate/Condition.java
+++ src/main/java/com/goodworkalan/deviate/Condition.java
@@ -1,8 +1,22 @@
 package com.goodworkalan.deviate;
 
-// TODO Document.
-public interface Condition
-{
-    // TODO Document.
+/**
+ * A condition to meet.
+ * <p>
+ * Conditions are mapped to keys, so that during a test, a map of keys to
+ * objects are provided, and the object given to the codition is the object
+ * mapped to the same key as the condition.
+ * 
+ * @author Alan Gutierrez
+ */
+public interface Condition {
+    /**
+     * Test that the given <code>object</code> meets this condition.
+     * 
+     * @param object
+     *            The object to test.
+     * @return <code>true</code> if the object meets the condition,
+     *         <code>false</code> otherwise.
+     */
     public boolean test(Object object);
 }
--- src/main/java/com/goodworkalan/deviate/RuleSetBuilderList.java
+++ src/main/java/com/goodworkalan/deviate/RuleSetBuilderList.java
@@ -3,7 +3,21 @@ package com.goodworkalan.deviate;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Document.
+/**
+ * This is an interface I created in order to have a single interface
+ * that I could pass in the Paste builder language, that would populate
+ * a lot of different sets, or populate the conditions for a handful of
+ * different routes that had been split by an or.
+ * <p>
+ * This is outgiong.
+ * <p>
+ * Because I can just as easily pass around a list or a list of lists in Paste
+ * and this interface won't exsit to confuse me.
+ * 
+ * @author Alan Gutierrez
+ *
+ * @param <T> The type conditionally mapped.
+ */
 public class RuleSetBuilderList<T> implements RuleSetBuilder<T>
 {
     // TODO Document.
